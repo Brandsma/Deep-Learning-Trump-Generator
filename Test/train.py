25a621597a1a479cd0c124220f725898746acef2
Brandsma <13336004+Brandsma@users.noreply.github.com>
2021-03-12 16:13:13 +0100

    added more alice in wonderland vibes

--- Test/train.py
+++ Test/train.py
@@ -44,4 +44,4 @@ checkpoint = ModelCheckpoint(
     filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
 callbacks_list = [checkpoint]
 # fit the model
-model.fit(X, y, epochs=20, batch_size=128, callbacks=callbacks_list)
+model.fit(X, y, epochs=2, batch_size=128, callbacks=callbacks_list)
