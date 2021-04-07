73fccd53e16a0cb23beb43281de98b1b067e1c2b
Brandsma <13336004+Brandsma@users.noreply.github.com>
2021-04-07 14:21:36 +0200

    Updated markov model

--- Markov Model/main.py
+++ Markov Model/main.py
@@ -1,5 +1,7 @@
 import markovify
 import pandas as pd
+import re
+import html
 
 
 def load_data(filename):
@@ -11,23 +13,49 @@ def preprocessing(df):
     pass
 
 
-def main(input_file: str, word_count: int = 5):
+def train(input_file: str):
     df = load_data(input_file)
     cur_text = ""
+    link_pattern = re.compile("http\S+")
     for idx in range(len(df["text"])):
-        cur_text += df["text"][idx].replace("\n", " ")
-    data_model = markovify.Text(cur_text)
+        if df["isRetweet"][idx] == "t":
+            continue
+        if 'http' in df["text"][idx]:
+            sample = re.sub(link_pattern, "", df["text"][idx])
+        else:
+            sample = df["text"][idx].replace("\n", " ")
 
-    for idx in range(word_count):
-        print(idx)
-        print(data_model.make_short_sentence(280))
-        print('\n')
+        cur_text += sample.replace("\n", " ")
+    data_model = markovify.Text(cur_text, state_size=3)
+    model_json = data_model.to_json()
+
+    with open("model.json", 'w') as f:
+        f.write(model_json)
+
+
+def predict(word_count: int = 5):
+    data_model = None
+    with open("model.json", 'r') as f:
+        data_model = markovify.Text.from_json(f.read())
+    if data_model is None:
+        print("Error: data model did not load")
+
+    with open("./markov_output.txt", "w", encoding="utf-8") as f:
+        for idx in range(word_count):
+            print(str(idx + 1) + "/" + str(word_count))
+            f.write(data_model.make_short_sentence(280))
+            f.write('\n')
 
 
 if __name__ == "__main__":
-    filename = "../tweets_01-08-2021.csv"
-    word_count = 5
-    main(filename, word_count)
+    filename = "../all.csv"
+    IsTraining = False
+
+    if IsTraining:
+        train(filename)
+
+    word_count = 100
+    predict(word_count)
 
 # Markov chain very basic model
 # Read all tweets, make a transition table
